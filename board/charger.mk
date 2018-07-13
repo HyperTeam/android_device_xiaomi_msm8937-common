788c129383945e1bdbcdf54c1eb79fd5252cc8e8
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-13 10:06:47 +0200

    msm8937-common: AICPfy

--- board/charger.mk
+++ board/charger.mk
@@ -1,3 +1,6 @@
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
