9ca29c09d63d9a141c923853083e421392bbb94d
karthick111 <karthickrko61@gmail.com>
2018-08-01 07:19:51 +0200

    msm8937-common: Remove sensors.msm8937

--- product/sensors.mk
+++ product/sensors.mk
@@ -12,8 +12,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
-    sensors.msm8937
+    android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
