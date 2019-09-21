30f7191d1b53b62351d33552725c54eccdd159c9
Roshan Pius <Roshan Pius>
2019-09-21 02:54:02 +0000

    msm8937-common: Turn off SoftAP MAC randomization

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -195,6 +195,7 @@ TARGET_DISABLE_WCNSS_CONFIG_COPY := true
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 # Inherit the common proprietary files
 -include vendor/xiaomi/msm8937-common/BoardConfigVendor.mk
