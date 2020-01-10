608cd1840b861f3a46bb664724d2413b60e9c35a
dianlujitao <dianlujitao@lineageos.org>
2020-01-10 15:22:33 +0000

    msm8937-common: Force enable mobile network settings v2
     * AOSP defaults to true but 10 QC telephony extension sets it to false.
     * Restore AOSP behavior by enabling it with a system property which has
       higher priority over global system settings which is used by QCOM w/o
       modifying the jar.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -138,7 +138,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    persist.vendor.data.mode=concurrent
+    persist.vendor.data.mode=concurrent \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
